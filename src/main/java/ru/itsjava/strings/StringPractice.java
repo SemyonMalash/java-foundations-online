a6f5d507f514f2ec6bd23ca26f0edf7a7887680a
SemyonMalash <9217861121@mail.ru>
2021-09-29 13:49:26 +0300

    Practice

---
+++ src/main/java/ru/itsjava/strings/StringPractice.java
@@ -0,0 +1,39 @@
+package ru.itsjava.strings;
+
+public class StringPractice {
+    public static void main(String[] args) {
+
+//        String name = "Vitaliy";
+//        String copyName = "Vitaliy";
+//        System.out.println("(name == copyName) = " + (name == copyName));
+//
+//        String constructorName = new String("Vitaliy");
+//        System.out.println("(constructorName == name) = " + (constructorName == name));
+//
+//        String internConstructorName = constructorName.intern();
+//        System.out.println("(internConstructorName == name) = " + (internConstructorName == name));
+
+
+        String str = "Я строка";
+
+        System.out.println("str.length() = " + str.length());
+        System.out.println("str.isEmpty() = " + str.isEmpty());
+
+        System.out.println("str.equals(\"Я строкА\") = " + str.equals("Я строкА"));
+        System.out.println("str.equalsIgnoreCase(\"Я строкА\") = " + str.equalsIgnoreCase("Я строкА"));
+
+        String[] strs = str.split(" ");
+        System.out.println(strs[0]);
+        System.out.println(strs[1]);
+
+
+
+        StringBuilder stringBuilderStr = new StringBuilder(str);
+        for (int i = 0; i < 1_000_000; i++) {
+            stringBuilderStr.append('!');
+        }
+
+        System.out.println(stringBuilderStr.toString());
+    }
+
+}

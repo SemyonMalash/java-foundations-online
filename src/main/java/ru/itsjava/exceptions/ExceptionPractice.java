c0418e19ff8fddf30eb3ddb5b2ca920a1ec3b0a9
SemyonMalash <9217861121@mail.ru>
2021-11-01 11:42:22 +0300

    Practice

---
+++ src/main/java/ru/itsjava/exceptions/ExceptionPractice.java
@@ -0,0 +1,42 @@
+package ru.itsjava.exceptions;
+
+import java.io.IOException;
+
+public class ExceptionPractice {
+
+    public static void main(String[] args) {
+
+//        throw new RuntimeException("111");
+//        throw new Exception("111");
+
+//        try {
+//            System.out.println("BEGIN");
+////            throw new IOException();
+//        } catch (Exception e) {
+//            System.out.println("CATCH");
+////            e.printStackTrace();
+//        } finally {
+//            System.out.println("FINALLY");
+//        }
+//        System.out.println("END");
+
+
+        try {
+            throw new RuntimeException();
+        } catch (RuntimeException runtimeException) {
+            System.out.println("Runtime exception");
+        } catch (Throwable throwable) {
+            System.out.println("Throwable");
+        }
+
+        Calculator calculator = new Calculator();
+
+        try {
+            System.out.println("calculator.division(5, 0) = " + calculator.division(5, 0));
+        } catch (CalculatorException ce) {
+            System.out.println("!!!");
+            ce.printStackTrace();
+        }
+
+    }
+}

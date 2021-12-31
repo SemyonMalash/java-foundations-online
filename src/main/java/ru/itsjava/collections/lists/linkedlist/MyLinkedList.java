ec10c9c304499de9e0b105d4cc80273c8e7ccea6
SemyonMalash <floan732@gmail.com>
2021-12-31 13:31:25 +0300

    Homework

--- src/main/java/ru/itsjava/collections/lists/linkedlist/MyLinkedList.java
+++ src/main/java/ru/itsjava/collections/lists/linkedlist/MyLinkedList.java
@@ -45,28 +45,149 @@ public class MyLinkedList {
         return true;
     }
 
+    //Метод с модификатором доступа public, тип boolean, название remove, параметр тип Object, название o
     public boolean remove(Object o) {
-        return false;
+        //Если первый элемент списка равен null, возвращаем false
+        if (head == null) return false;
+        //Если значение первого элемента списка равно значению удаляемого
+        if (head.getValue().equals(o)) {
+            //Первый элемент списка становится равен следующему
+            head = head.getNext();
+            //Возвращаем true
+            return true;
+        }
+        //Если второй элемент списка равен null, возвращаем false
+        if (head.getNext() == null) return false;
+        //Объявляем переменную curNode типа Node, присваиваем ей значение первого элемента списка
+        Node curNode = head;
+        //Объявляем переменную prevNode типа Node, присваиваем ей значение первого элемента списка
+        Node prevNode = head;
+        //Присваиваем текущему элементу списка значение следующиего, пока значение следующего не равно null
+        while ((curNode = curNode.getNext()) != null) {
+            //Если значение текущего элемента равно значению удаляемого
+            if (curNode.getValue().equals(o)) {
+                //Выходим из цикла
+                break;
+            }
+            //Присваиваем переменной prevNode значение следующего элемента
+            prevNode = prevNode.getNext();
+        }
+        //Если текущий элемент равен null, возвращаем false
+        if (curNode == null) return false;
+        //Меняем ссылку предыдущего элемента на ссылку текущего элемента
+        prevNode.setNext(curNode.getNext());
+        //Делаем ссылку текущего элемента равной null
+        curNode.setNext(null);
+        //Возвращаем правду
+        return true;
     }
 
     public void clear() {
-
+        Node curNode = head;
+        while (curNode != null) {
+            curNode.setValue(null);
+            Node nextNode = curNode.getNext();
+            curNode.setNext(null);
+            curNode = nextNode;
+        }
+        head = null;
     }
 
     public Object get(int index) {
+        Node curNode = head;
+        int count = 0;
+
+        while (curNode != null) {
+            if (count == index) {
+                return curNode;
+            }
+            curNode = curNode.getNext();
+            count++;
+        }
         return null;
     }
 
     public Object set(int index, Object element) {
-        return null;
+        checkIndex(index);
+        Node curNode = head;
+        int count = 0;
+
+        while (curNode != null) {
+            if (count == index) {
+                break;
+            }
+            curNode = curNode.getNext();
+            count++;
+        }
+        curNode.setValue(element);
+        return true;
     }
 
     public void add(int index, Object element) {
-
     }
 
+    // СТРОКА1 -> null
+    // СТРОКА1 -> СТРОКА2 -> СТРОКА3 -> null
+    //Объявляем метод с модификатором доступа public, тип Object, название remove, параметр тип int, название index
     public Object remove(int index) {
-        return null;
+        //Проверяем корректность индекса
+        checkIndex(index);
+        //Если индекс равен нулю
+        if (index == 0) {
+            //Объявляем переменную типа Object с названием resValue и значением первого элемента
+            //Запоминаем значение первого элемента
+            Object resValue = head.getValue();
+            //Первый элемент делаем равным второму
+            head = head.getNext();
+            //Возвращаем resValue
+            return resValue;
+        }
+        //Объявляем переменную типа Node с названием curNode, делаем её равной первому элементу
+        Node curNode = head;
+        //Объявляем переменную типа Node с названием prevNode, делаем её равной первому элементу
+        Node prevNode = head;
+        //Объявляем переменную типа int с названием count, делаем её равной нулю
+        int count = 0;
+        //Присваиваем текущему элементу списка значение следующего, пока значение следующего не равно null
+        while ((curNode = curNode.getNext()) != null) {
+            //Увеличиваем счётчик на 1
+            count++;
+            //Если счётчик равен индексу
+            if (count == index) {
+                //Выходим из цикла
+                break;
+            }
+            //Предыдущий элемент равен следующиему
+            prevNode = prevNode.getNext();
+        }
+        //Объявляем переменную типа Object с названием resValue и значением текущего элемента
+        //Запоминаем значение текущего элемента
+        Object resValue = curNode.getValue();
+        //Если элемент следующий за текущим равен null
+        if (curNode.getNext() == null) {
+            //Делаем ссылку предыдущего элемента равной null
+            prevNode.setNext(null);
+            //Иначе меняем ссылку предыдущего элемента на ссылку текущего элемента
+        } else {
+            prevNode.setNext(curNode.getNext());
+            //Делаем ссылку текущего элемента равной null
+            curNode.setNext(null);
+        }
+        //Возвращаем значение удалённого элемента
+        return resValue;
+    }
+
+    private void checkIndex(int index) {
+        if (!isCorrectIndex(index)) {
+            throw new ArrayIndexOutOfBoundsException("Некорректный индекс");
+        }
+    }
+
+    private boolean isCorrectIndex(int index) {
+        if ((index > -1) && (index < size())) {
+            return true;
+        }
+        return false;
     }
 
     public int indexOf(Object o) {
--- src/main/java/ru/itsjava/collections/lists/linkedlist/MyLinkedListPractice.java
+++ src/main/java/ru/itsjava/collections/lists/linkedlist/MyLinkedListPractice.java
@@ -1,17 +1,43 @@
 package ru.itsjava.collections.lists.linkedlist;
 
+import java.util.LinkedList;
+
 public class MyLinkedListPractice {
 
     public static void main(String[] args) {
 
         MyLinkedList list = new MyLinkedList();
 
-        list.add('1');
-        list.add('2');
-        list.add('3');
+        new LinkedList<>().clear();
+
+        list.add(11);
+        list.add(22);
+        list.add(null);
+        list.add(44);
+
+        System.out.println("list.get(0) = " + list.get(0));
+        System.out.println("list.get(1) = " + list.get(1));
+        System.out.println("list.get(2) = " + list.get(2));
+        System.out.println("list.get(3) = " + list.get(3));
+        System.out.println("list.get(4) = " + list.get(4));
+        System.out.println("list.get(5) = " + list.get(5));
+        System.out.println("list.set(2, 555) = " + list.set(2, 555));
         System.out.println(list);
 
-        System.out.println("list.size() = " + list.size());
-        System.out.println("list.contains('3') = " + list.contains('3'));
+//        System.out.println("list.remove(-1) = " + list.remove(-1));
+//        System.out.println("list.remove(0) = " + list.remove(0));
+//        System.out.println("list.remove(1) = " + list.remove(1));
+//        System.out.println("list.remove(2) = " + list.remove(2));
+//        System.out.println("list.remove(null) = " + list.remove(3));
+//        System.out.println("list.remove(4) = " + list.remove(4));
+
+
+//        System.out.println("list.size() = " + list.size());
+//        System.out.println("list.contains('3') = " + list.contains('3'));
+//
+//        System.out.println("list.remove(1) = " + list.remove(1));
+//        System.out.println("list = " + list);
+
+
     }
 }

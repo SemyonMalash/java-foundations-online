fff2ec9c568a9a730e6b3b56ab0b3fc53656e4cc
SemyonMalash <floan732@gmail.com>
2022-02-14 06:19:33 +0300

    Homework

--- src/main/java/ru/itsjava/collections/maps/MapPractice.java
+++ src/main/java/ru/itsjava/collections/maps/MapPractice.java
@@ -1,5 +1,6 @@
 package ru.itsjava.collections.maps;
 
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -38,36 +39,108 @@ public class MapPractice {
 
         Map<Man, Fruit> customers = new HashMap<>();
 
-        Man ivanov = new Man("Иванов");
+        Man anov = new Man("анов");
         Man sidorov = new Man("Сидоров");
-        Man javov = new Man("Джавов");
+        Man avov = new Man("авов");
         Man petrov = new Man("Петров");
-        Man dubov = new Man("Дубов");
+        Man adubov = new Man("адубов");
 
-        Fruit apple = new Fruit("Яблоко", 15);
-        Fruit orange = new Fruit("Апельсин", 20);
-        Fruit banana = new Fruit("Банан", 30);
-        Fruit melon = new Fruit("Дыня", 200);
-        Fruit apricot = new Fruit("Абрикос", 10);
+        Fruit apple = new Fruit("Абрикос", 15);
+        Fruit orange = new Fruit("АРбУз", 20);
+        Fruit banana = new Fruit("Арбуз", 30);
+        Fruit melon = new Fruit("арбуз", 200);
+        Fruit apricot = new Fruit("Дыня", 10);
 
-        customers.put(ivanov, apple);
+        customers.put(anov, apple);
         customers.put(sidorov, orange);
-        customers.put(javov, banana);
+        customers.put(avov, banana);
         customers.put(petrov, melon);
-        customers.put(dubov, apricot);
+        customers.put(adubov, apricot);
 
         System.out.println("customers.get(petrov) = " + customers.get(petrov));
 
-        System.out.println("customers.remove(ivanov, apple) = " + customers.remove(ivanov, apple));
-        System.out.println("customers.get(ivanov) = " + customers.get(ivanov));
+        //System.out.println("customers.remove(ivanov, apple) = " + customers.remove(ivanov, apple));
+        System.out.println("customers.get(ivanov) = " + customers.get(anov));
 
         System.out.println("customers.keySet() = " + customers.keySet());
         System.out.println("customers.values() = " + customers.values());
         System.out.println("customers.entrySet() = " + customers.entrySet());
 
-        //Можно и так, но не очень понимаю зачем, если есть toString :)
         for (Map.Entry<Man, Fruit> pair : customers.entrySet()) {
             System.out.println(pair.getKey() + ": " + pair.getValue());
         }
+
+        int keyCount = 0;
+        for (Man keyCustomers : customers.keySet()) {
+            if (keyCustomers.getName().length() > 5) {
+                keyCount++;
+            }
+        }
+        System.out.println(keyCount);
+
+        int melonCount = 0;
+        for (Fruit valueCustomers : customers.values()) {
+            if (valueCustomers.getName().equals("Дыня")) {
+                melonCount++;
+            }
+        }
+        System.out.println(melonCount);
+
+        int watermelonCount = 0;
+        for (Map.Entry<Man, Fruit> pair : customers.entrySet()) {
+            if (pair.getValue().getName().equals("Арбуз")) {
+                watermelonCount++;
+            } else {
+                System.out.println(pair.getKey() + " : " + pair.getValue());
+            }
+            if (pair.getValue().getName().equals("Арбуз") && watermelonCount > 2) {
+                System.out.println(pair.getKey() + " : " + pair.getValue());
+            }
+        }
+
+        System.out.println("__________________________");
+
+        int aCount = 0;
+        for (Map.Entry<Man, Fruit> pair : customers.entrySet()) {
+            if (pair.getKey().getName().charAt(0) == 'а') {
+                aCount++;
+            } else {
+                System.out.println(pair.getKey() + " : " + pair.getValue());
+            }
+            if (pair.getKey().getName().charAt(0) == 'а' && aCount > 2) {
+                System.out.println(pair.getKey() + " : " + pair.getValue());
+            }
+        }
+
+        System.out.println("__________________________");
+
+        System.out.println("customers.entrySet() = " + customers.entrySet());
+
+        System.out.println("__________________________");
+
+        for (Map.Entry<Man, Fruit> pair : customers.entrySet()) {
+            if (pair.getValue().getName().equals("Арбуз") || pair.getValue().getName().equals("арбуз") || pair.getValue().getName().equals("АРбУз")) {
+                System.out.println(pair.getKey() + " : " + pair.getValue());
+            }
+            break;
+        }
+
+        Map<Man, Fruit> customers2 = new HashMap<>();
+        for (Map.Entry<Man, Fruit> pair : customers.entrySet()) {
+            if (pair.getValue().getName().equals("Арбуз") || pair.getValue().getName().equals("Дыня")) {
+                customers2.put(pair.getKey(), pair.getValue());
+            }
+        }
+        System.out.println("customers2.entrySet() = " + customers2.entrySet());
+
+        int count = 0;
+        int sumWeight = 0;
+        int mediumWeight = 0;
+        for (Fruit valueCustomers : customers.values()) {
+            sumWeight += valueCustomers.getWeight();
+            count++;
+        }
+        mediumWeight = sumWeight / count;
+        System.out.println("mediumWeight = " + mediumWeight);
     }
 }

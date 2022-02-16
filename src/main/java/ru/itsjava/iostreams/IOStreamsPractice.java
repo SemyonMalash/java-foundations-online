1b967b69ac9e76f58ebf8c38ebaa645718c16ffd
SemyonMalash <floan732@gmail.com>
2022-02-16 19:29:10 +0300

    Homework

--- src/main/java/ru/itsjava/iostreams/IOStreamsPractice.java
+++ src/main/java/ru/itsjava/iostreams/IOStreamsPractice.java
@@ -1,6 +1,7 @@
 package ru.itsjava.iostreams;
 
 import java.io.*;
+import java.util.ArrayList;
 
 public class IOStreamsPractice {
     public static void main(String[] args) throws IOException {
@@ -27,20 +28,57 @@ public class IOStreamsPractice {
 //            exception.printStackTrace();
 //        }
 
-        File file = new File("src/main/resources/file.txt");
+//        File file = new File("src/main/resources/file.txt");
+//
+//        try (PrintWriter printWriter = new PrintWriter(file)) {
+//            printWriter.println("1324525");
+//            printWriter.println("13245257677777");
+//            printWriter.println("1324525999999");
+//        } catch (FileNotFoundException fileNotFoundException) {
+//            fileNotFoundException.printStackTrace();
+//        }
+//
+//        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+//            System.out.println("reader.readLine() = " + reader.readLine());
+//            System.out.println("reader.readLine() = " + reader.readLine());
+//            System.out.println("reader.readLine() = " + reader.readLine());
+//        }
 
-        try (PrintWriter printWriter = new PrintWriter(file)) {
-            printWriter.println("1324525");
-            printWriter.println("13245257677777");
-            printWriter.println("1324525999999");
-        } catch (FileNotFoundException fileNotFoundException) {
-            fileNotFoundException.printStackTrace();
+        File file1 = new File("src/main/resources/file1.txt");
+        try (PrintWriter printWriter = new PrintWriter(file1)) {
+            printWriter.println("11111");
+        }
+        File file2 = new File("src/main/resources/file2.txt");
+        try (PrintWriter printWriter = new PrintWriter(file2)) {
+            printWriter.println("22222");
+        }
+        try (BufferedReader reader = new BufferedReader(new FileReader(file1))) {
+            System.out.println(reader.readLine());
+        }
+        try (BufferedReader reader = new BufferedReader(new FileReader(file2))) {
+            System.out.println(reader.readLine());
         }
 
-        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-            System.out.println("reader.readLine() = " + reader.readLine());
-            System.out.println("reader.readLine() = " + reader.readLine());
-            System.out.println("reader.readLine() = " + reader.readLine());
+        ArrayList change = new ArrayList();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(file1))) {
+            change.add(0, reader.readLine());
+        }
+        try (BufferedReader reader = new BufferedReader(new FileReader(file2))) {
+            change.add(1, reader.readLine());
+        }
+        try (PrintWriter printWriter = new PrintWriter(file1)) {
+            printWriter.println(change.get(1));
+        }
+        try (PrintWriter printWriter = new PrintWriter(file2)) {
+            printWriter.println(change.get(0));
+        }
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(file1))) {
+            System.out.println(reader.readLine());
+        }
+        try (BufferedReader reader = new BufferedReader(new FileReader(file2))) {
+            System.out.println(reader.readLine());
         }
     }
 }
